a811745a6298f4218a101e2559b8b0376dc3164b
Bojan Manasijevic <bojanja2002@gmail.com>
2021-07-30 17:04:14 +0200

    Prvi commit

--- moj-site/index.php
+++ moj-site/index.php
@@ -6,7 +6,7 @@ echo "Today is " . date("d.m.Y") . "<br>";
 echo "The time is " . date("h:i:sa") . "<br>";
 echo "Today is " . date("l") . "<br><br><br>";
 
-echo "Verzija: 1 <br><br>";
+echo "Verzija: 2 <br><br>";
 
 echo "Bojan";
 
